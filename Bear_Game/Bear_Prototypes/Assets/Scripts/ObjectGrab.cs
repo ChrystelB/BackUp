aa14ff56bdf5ba97c933e62c89649e6920ca5284
Chrystel <chrystelbardsley@gmail.com>
2017-09-10 23:38:29 -0600

    Death/Transportation scripts implemented.

---
+++ Bear_Game/Bear_Prototypes/Assets/Scripts/ObjectGrab.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectGrab : MonoBehaviour
+{
+
+
+	public GameObject player;
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		print("PickUp");
+	}
+			
+	public void SetParent(GameObject newParent)
+	{
+				
+		player.transform.parent = newParent.transform;
+
+				
+		Debug.Log("Player's Parent: " + player.transform.parent.name);
+
+				
+		if (newParent.transform.parent != null)
+		{
+					
+			Debug.Log("Player's Grand parent: " + player.transform.parent.parent.name);
+		}
+
+	}
+		
+	
+}
