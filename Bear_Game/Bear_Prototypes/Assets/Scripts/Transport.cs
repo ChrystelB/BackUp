055b8cf122fef495a802ae52df957ed0da111d63
Chrystel <chrystelbardsley@gmail.com>
2017-09-18 22:48:36 -0600

    New Scene called Bear_Cave

--- Bear_Game/Bear_Prototypes/Assets/Scripts/Transport.cs
+++ Bear_Game/Bear_Prototypes/Assets/Scripts/Transport.cs
@@ -4,35 +4,13 @@ using UnityEngine;
 
 public class Transport : MonoBehaviour {
 
-	//public Transform transportArea;
-	public Transform goHere;
-	private GameObject player;
-	public static bool canTransport = true;
-	
-	//public GameObject art;
+	public Transform player;
+	public Transform SpawnPoint;
 
-	void Start (){
-//		ZapPoint.Reappear += Transfer;
-		player = GameObject.FindGameObjectWithTag("Player");
-	}
-
-
-	/*void Transfer (){
-
-		transform.position = transportArea.position;
-
-	}*/
-
-	void OnTriggerEnter (Collider other) {
-		if (other.tag == "Player" && canTransport == true){
-			player.transform.position = goHere.transform.position;
-			canTransport = false;
-			print("Enter");
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player") {
+			player.transform.position = SpawnPoint.transform.position;
 		}
-	}
-
-	void OnTriggerExit () {
-		canTransport = true;
-		print("Exit");
 	}
 }
