f6c74be91c6a5ed66d903b02e424ee8be49525fa
Chrystel <chrystelbardsley@gmail.com>
2017-09-20 22:21:50 -0600

    Compiled everything into a zip file, and turned it in.
    Wow, 4221 files huh?

---
+++ Bear_Game/Bear_Prototypes/Temp/StagingArea/Data/il2cppOutput/AssemblyU2DCSharp_CreateTrigger2237538136.h
@@ -0,0 +1,93 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+
+#include "UnityEngine_UnityEngine_MonoBehaviour1158329972.h"
+
+// UnityEngine.Transform
+struct Transform_t3275118058;
+// UnityEngine.GameObject
+struct GameObject_t1756533147;
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// CreateTrigger
+struct  CreateTrigger_t2237538136  : public MonoBehaviour_t1158329972
+{
+public:
+	// UnityEngine.Transform CreateTrigger::Spawnpoint1
+	Transform_t3275118058 * ___Spawnpoint1_2;
+	// UnityEngine.Transform CreateTrigger::Spawnpoint2
+	Transform_t3275118058 * ___Spawnpoint2_3;
+	// UnityEngine.Transform CreateTrigger::Spawnpoint3
+	Transform_t3275118058 * ___Spawnpoint3_4;
+	// UnityEngine.Transform CreateTrigger::Spawnpoint4
+	Transform_t3275118058 * ___Spawnpoint4_5;
+	// UnityEngine.GameObject CreateTrigger::Prefab
+	GameObject_t1756533147 * ___Prefab_6;
+
+public:
+	inline static int32_t get_offset_of_Spawnpoint1_2() { return static_cast<int32_t>(offsetof(CreateTrigger_t2237538136, ___Spawnpoint1_2)); }
+	inline Transform_t3275118058 * get_Spawnpoint1_2() const { return ___Spawnpoint1_2; }
+	inline Transform_t3275118058 ** get_address_of_Spawnpoint1_2() { return &___Spawnpoint1_2; }
+	inline void set_Spawnpoint1_2(Transform_t3275118058 * value)
+	{
+		___Spawnpoint1_2 = value;
+		Il2CppCodeGenWriteBarrier(&___Spawnpoint1_2, value);
+	}
+
+	inline static int32_t get_offset_of_Spawnpoint2_3() { return static_cast<int32_t>(offsetof(CreateTrigger_t2237538136, ___Spawnpoint2_3)); }
+	inline Transform_t3275118058 * get_Spawnpoint2_3() const { return ___Spawnpoint2_3; }
+	inline Transform_t3275118058 ** get_address_of_Spawnpoint2_3() { return &___Spawnpoint2_3; }
+	inline void set_Spawnpoint2_3(Transform_t3275118058 * value)
+	{
+		___Spawnpoint2_3 = value;
+		Il2CppCodeGenWriteBarrier(&___Spawnpoint2_3, value);
+	}
+
+	inline static int32_t get_offset_of_Spawnpoint3_4() { return static_cast<int32_t>(offsetof(CreateTrigger_t2237538136, ___Spawnpoint3_4)); }
+	inline Transform_t3275118058 * get_Spawnpoint3_4() const { return ___Spawnpoint3_4; }
+	inline Transform_t3275118058 ** get_address_of_Spawnpoint3_4() { return &___Spawnpoint3_4; }
+	inline void set_Spawnpoint3_4(Transform_t3275118058 * value)
+	{
+		___Spawnpoint3_4 = value;
+		Il2CppCodeGenWriteBarrier(&___Spawnpoint3_4, value);
+	}
+
+	inline static int32_t get_offset_of_Spawnpoint4_5() { return static_cast<int32_t>(offsetof(CreateTrigger_t2237538136, ___Spawnpoint4_5)); }
+	inline Transform_t3275118058 * get_Spawnpoint4_5() const { return ___Spawnpoint4_5; }
+	inline Transform_t3275118058 ** get_address_of_Spawnpoint4_5() { return &___Spawnpoint4_5; }
+	inline void set_Spawnpoint4_5(Transform_t3275118058 * value)
+	{
+		___Spawnpoint4_5 = value;
+		Il2CppCodeGenWriteBarrier(&___Spawnpoint4_5, value);
+	}
+
+	inline static int32_t get_offset_of_Prefab_6() { return static_cast<int32_t>(offsetof(CreateTrigger_t2237538136, ___Prefab_6)); }
+	inline GameObject_t1756533147 * get_Prefab_6() const { return ___Prefab_6; }
+	inline GameObject_t1756533147 ** get_address_of_Prefab_6() { return &___Prefab_6; }
+	inline void set_Prefab_6(GameObject_t1756533147 * value)
+	{
+		___Prefab_6 = value;
+		Il2CppCodeGenWriteBarrier(&___Prefab_6, value);
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
